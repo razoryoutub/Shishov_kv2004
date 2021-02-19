d56d699960b17a83b98d83189622331efcdc29a3
Oleg <razoryoutub@mail.ru>
2021-02-19 19:40:50 +0300

    add LR 1

---
+++ Shishov_kv2004_lr2o.py
@@ -0,0 +1,39 @@
+number = int(15)
+print(int(number))
+number = number + 10
+print(int(number))
+number = 36 * 15
+print(int(number))
+number = 12 - (42 / 7)
+print(int(number))
+number += 10
+print(int(number))
+number *= 3
+print(int(number))
+number = 71 / 3
+print(int(number))
+
+
+count = int(0)
+count += 1
+print(int(count))
+count -= 1
+print(int(count))
+
+
+result = 'hello'
+print(result)
+result += ' again ' + result
+print(result)
+result = 'the value is: ' + str(count)
+print(result)
+result = ''
+print(result)
+
+
+yesNo = False
+print(yesNo)
+anotherBool = True
+print(anotherBool)
+yesNo = not anotherBool
+print(yesNo)
